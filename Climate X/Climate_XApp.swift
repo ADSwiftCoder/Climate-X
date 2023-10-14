1edf611b4612a85db07c0ab750809aff7ba54d6d
Adrian Roger Menezes <adrianrogermenezes10@gmail.com>
2023-10-14 19:00:07 +0400

    Initial Commit

---
+++ Climate X/Climate_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Climate_XApp.swift
+//  Climate X
+//
+//  Created by Adrian Menezes on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Climate_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
